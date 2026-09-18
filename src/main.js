51633f3f2c2c3183eb23c8812d3a7111f3a733d8
fanwenhui <QlFwh@outlook.com>
2026-09-18 16:16:40 +0800

    静态资源引入

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-//测试接口函数
-import { getCategory } from '@/apis/testAPI'
-getCategory().then((res) => {
-  console.log(res)
-})
+//引入初始化样式文件
+import '@/styles/common.scss'
+
+import '@/styles/com'
 const app = createApp(App)
 
 app.use(createPinia())
