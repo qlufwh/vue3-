017ad03f3b0332dbeb5e09da94ffc74175cae817
fanwenhui <QlFwh@outlook.com>
2026-09-20 00:25:01 +0800

    整体结构拆分和分类实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,17 @@
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-    <div>
-        我是Home
+    <div class="container">
+        <HomeCategory />
+        <HomeBanner />
     </div>
-    <div style="height: 1200px;"></div>
-</template>>
+    <HomeNew />
+    <HomeHot />
+    <HomeProduct />
+</template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import LayoutNav from './components/LayoutNay.vue'
+import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
